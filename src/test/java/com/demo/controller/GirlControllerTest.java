4dfe3f305b2655852011807be899643a7c6d892c
caramel <yamifang1028@gmail.com>
2018-03-11 20:50:56 +0800

    增加单元测试

---
+++ src/test/java/com/demo/controller/GirlControllerTest.java
@@ -0,0 +1,35 @@
+package com.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author yami fang
+ * @since 20:34 2018/3/11
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GirlControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void updateGirl() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.put("/girls/3")
+                .param("age","25").param("cupSize","c"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
