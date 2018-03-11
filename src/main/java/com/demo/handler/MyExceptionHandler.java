0ca34d6769d9143abed1c5f6b2647fa83941ffe5
caramel <yamifang1028@gmail.com>
2018-03-11 19:48:29 +0800

    打印异常日志

--- src/main/java/com/demo/handler/MyExceptionHandler.java
+++ src/main/java/com/demo/handler/MyExceptionHandler.java
@@ -2,6 +2,8 @@ package com.demo.handler;
 
 import com.demo.Utils.ResultUtils;
 import com.demo.entity.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MyExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyExceptionHandler.class);
+
     @ExceptionHandler(RuntimeException.class)
     public Result handle(Exception e){
+        logger.error("异常", e);
         return ResultUtils.error(100,e.getMessage());
     }
 }
